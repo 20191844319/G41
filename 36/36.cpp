b847e83670dae1c71a7c7ad9d6cea2f3da123e13
20191844322 <2590537051@qq.com>
2019-10-22 08:32:56 +0800

    add 35 and 36 project

---
+++ 36/36.cpp
@@ -0,0 +1,17 @@
+// 36.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+int main(int argc, char* argv[])
+{
+	char c1='C',c2='h',c3='i',c4='n',c5='a';
+	putchar(c1=c1+4);
+	putchar(c2=c2+4);
+	putchar(c3=c3+4);
+	putchar(c4=c4+4);
+	putchar(c5=c5+4);
+	putchar('\n');
+	return 0;
+}
+
