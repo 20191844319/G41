92a57113ecb5f897c5a46fe3ee23b6c4cfad7f77
20191844322 <2590537051@qq.com>
2019-11-13 17:08:44 +0800

    第五章

---
+++ c5/5章2题/522/522.cpp
@@ -0,0 +1,24 @@
+// 522.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include<math.h>
+
+int main(int argc, char* argv[])
+{
+	int sign=1,count=0;
+	double pi=0.0,n=1.0,term=1.0;
+	while(fabs(term)>=1e-8)
+	{
+		pi=pi+term;
+		n=n+2;
+		sign=-sign;
+		term=sign/n;
+		count++;
+	}
+	pi=pi*4;
+	printf("pi=%10.8f\n",pi);
+	printf("count=%d\n",count);
+	return 0;
+}
+
