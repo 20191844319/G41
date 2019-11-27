a7b2912ee9e1af5fda4c609f8c4399f47de08e6f
20191844317 <dong1196750277@163.com>
2019-11-27 21:51:13 +0800

    7.8

---
+++ c7/7章8题/7 8.cpp
@@ -0,0 +1,23 @@
+// 7 8.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include<string.h>
+int main(int argc, char* argv[])
+{void insert(char []);
+char str[80];
+printf("ÇëÊäÈëÊý×Ö:\n");
+scanf("%s",str);
+insert(str);
+return 0;
+}
+
+void insert (char str[])
+{int i;
+for(i=strlen(str);i>0;i--){
+	str[2*i]=str[i];
+	str[2*i-1]=' ';
+}
+printf("output:\n%s\n",str);
+}
+
